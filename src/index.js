019efb027f3846557fb4a311e04f1c33d886acd0
InjamHossan <injamhossanmamun@gmail.com>
2026-01-03 11:22:09 +0600

    vercel link

--- src/index.js
+++ src/index.js
@@ -21,7 +21,8 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
-  'https://studymate-ih.netlify.app'
+  'https://studymate-ih.netlify.app',
+  'https://studymate-ih.vercel.app'
 ];
 
 const corsOptions = {
